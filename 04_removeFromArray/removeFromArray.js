3fbf4cba21da43fbc1fdbe2491899e5caf227a95
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-11 21:51:48 +0100

    Complete Exercise #4 | 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(initialArray, ...removeItem) {
+    
+    for (i=0; i <= removeItem.length; i++) {
+        let index = initialArray.indexOf(removeItem[i]);
+        if (index > -1) {
+            initialArray.splice(index,1);
+            i--;
+        }
+    }
 
+    return initialArray;
 };
 
 // Do not edit below this line
