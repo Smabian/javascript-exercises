54df9a4b780fd78b48223061aa0d1d7b59cf3d90
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-11 20:48:55 +0100

    Complete Exercise #3 | 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(initialString) {
+    let reverseString = "";
+    let stringLength = initialString.length;
+    
+    for(i=stringLength; i>=0; i--) {
+        reverseString += initialString.charAt(i);
+    }
+
+    return reverseString;
 
 };
 
