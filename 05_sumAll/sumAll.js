e8112c5683e1c527c3bac97f1da0c0687d335736
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-11 22:05:56 +0100

    Complete Exercise #5 | 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
+    let sumAll = 0;
+    let smallNumber;
+    let largeNumber;
+
+    if(number1 <= 0 || number2 <= 0 || "number" !== typeof number1 || "number" !== typeof number2 ) {
+        return sumAll = "ERROR";
+    }
+
+    if(number1 < number2) {
+        smallNumber = number1;
+        largeNumber = number2;
+    } else {
+        smallNumber = number2;
+        largeNumber = number1;
+    }
+
+    for(i=smallNumber; i <= largeNumber; i++) {
+        sumAll += i;
+    }
+    return sumAll;
 };
 
 // Do not edit below this line
