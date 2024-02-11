7f5c94248467a8ae0705e3dece03a807bc0e1d30
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-11 20:41:30 +0100

    Complete Exercise #2 | 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, repeat) {
+    let outputText = "";
 
+    if(repeat < 0){
+       outputText = "ERROR";
+    }
+
+    for(i=repeat;i>0;i--) {
+        outputText += text;
+    }
+    return outputText;
 };
 
 // Do not edit below this line
