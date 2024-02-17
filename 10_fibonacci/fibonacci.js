5875b7d7828ab8d47fb0d011eb78c97a60c0571f
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-17 16:42:42 +0100

    Complete Exercise #10 | 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
 
+    if (value < 0) {
+        return "OOPS";
+    } 
+
+    let fibonacci = [0,1];
+
+    for (i=0; fibonacci.length <= value; i++) {
+        fibonacci[i+2] = fibonacci [i] + fibonacci [i+1];
+    }
+
+    return fibonacci[value];
 };
 
 // Do not edit below this line
