a50876899571cc636cb128e4dceebd5b6d2665f2
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-17 09:35:54 +0100

    Complete Exercise #8 | 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr === undefined || arr.length == 0) {
+    return 0;
+  } 
+  return arr.reduce((sum, first) => sum += first);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, first) => sum = sum * first);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    let sum = 1;
+    for (i = a; i>0; i--){
+      sum = sum * i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
