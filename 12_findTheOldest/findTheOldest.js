81e12a04a24003c37b42a6fe33a66451c768f679
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-17 17:25:12 +0100

    Complete Exercise #12 | 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentAge = 0;
+    let oldestAge = 0;
+    let oldestPerson;
+    let today = new Date();
 
+    for (name in people) {
+        if(people[name].hasOwnProperty("yearOfDeath")) {
+            currentAge = people[name].yearOfDeath - people[name].yearOfBirth;
+        } else {
+            currentAge = today.getFullYear() - people[name].yearOfBirth;
+        }
+        if (currentAge > oldestAge) {
+            oldestAge = currentAge;
+            oldestPerson = people[name];
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
