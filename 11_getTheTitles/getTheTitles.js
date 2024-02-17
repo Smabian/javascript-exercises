490c74aa80a2e6d2484848b4285e27690eb93997
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-17 17:02:05 +0100

    Complete Exercise #11 | 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const array = [];
 
-};
+    for (title in books) {
+        array[title] = books[title].title;
+    }
+
+    return array;
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
