014886ff8e4acd586e6cc634013425eaa98d56c4
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-17 16:26:15 +0100

    Complete Exercise #9 | 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    text = text.toLowerCase();
+    text = text.replaceAll(" ", "");
+    text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+
+    let reverseText = "";
+
+    for(i=text.length; i >= 0; i--){
+        let letter = text.charAt(i);
+        reverseText = reverseText.concat(letter);
+    }
+
+    if (reverseText === text) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
